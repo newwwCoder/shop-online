9cbbf024b32a1ba59a9f64639b4245bd41937b4c
unknown <ayushibhardwaj288@gmail.com>
2020-08-04 19:07:01 +0530

    changes

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+
+const app=express();
+
+const mongoose=require('mongoose')
+
+app.use(express.urlencoded());
+const router=require('./routers/index')
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use('/',router)
+
+mongoose.connect('mongodb+srv://ayuhimi:Qm6WMt4W7b0lm36j@cluster0.ahpwd.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result=>{
+    app.listen(8000);
+    console.log('connected to mongodb');
+})
+.catch(err=>{
+    console.log(err);
+})
+
